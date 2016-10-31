2484bf26ba465ee50b8018d41f24c38cbe1a11c6
Kyle Holmberg <duskykmh@gmail.com>
2016-10-30 18:33:06 -0700

    Removed Milestone Report Directory - we'll keep that for ourselves.

--- FPL Scraper/fplscrape.py
+++ FPL Scraper/fplscrape.py
@@ -1,26 +1,32 @@
 import requests
+import json
 
-
+player_id = []
 all_data = {}
-api_error = {}
+errors = {}
 
-def set_filepath(file_from_cloud):
-    if platform.system() == "Mac"
+def parse_data(data):
+    json_parsed = json.loads(data)
+    print(json_parsed)
+
+def get_current_stats():
+    current_url = "https://fantasy.premierleague.com/drf/bootstrap-static"
+    r = requests.get(current_url)
 
 
-for i in range(594):
-    player_url = "https://fantasy.premierleague.com/drf/element-summary/{}".format(i)
-    print("Scraping Player #: " + i + ".\n")
-    r=requests.get(player_url)
+def get_old_stats(url_range):
+    for i in range(url_range):
+        player_url = "https://fantasy.premierleague.com/drf/element-summary/{}".format(i)
+        print(i)
+        r = requests.get(player_url)
 
-    # Skip empty pages
-    if r.status_code != 200:
-        api_error[i] = r.status_code
-        print("Not found.\n")
-        continue
+        # Skip empty pages
+        if r.status_code != 200:
+            errors[i] = r.status_code
+            print("Not found.\n")
+            continue
 
-    # Put player name as dictionary key. Remaining data is the key for each entry
-    temp = r.json()
-    player_name = temp['web_name']
-    print(player_name + "\n")
-    all_data[player_name] = temp
+        # Put player name as dictionary key. Remaining data is the key for each entry
+        temp = r.json()
+
+parse_data(all_data)
