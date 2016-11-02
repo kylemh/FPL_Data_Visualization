74bc69d62dd11a1f9d804c1015cabe87557bea5b
Kyle Holmberg <duskykmh@gmail.com>
2016-11-02 03:54:27 -0700

    Let's call this the REAL first commit. TODO: Create Local HTML File to Prevent Multiple Page Requests, General Refactoring, Implement Munging Methods, and CSV Conversion (plus Output)

--- FPL Scraper/fplscrape.py
+++ FPL Scraper/fplscrape.py
@@ -1,33 +1,61 @@
 import requests
-import json
 
-player_id = []
-all_data = {}
-errors = {}
-
-def parse_data(data):
-    json_parsed = json.loads(data)
-    print(json_parsed)
-
-def get_current_stats():
-    current_url = "https://fantasy.premierleague.com/drf/bootstrap-static"
-    r = requests.get(current_url)
+PLAYER_NAMES = []
+PLAYER_DATA_DICT = {}
 
 
-def get_old_stats(url_range):
-    for i in range(url_range):
-        player_url = "https://fantasy.premierleague.com/drf/element-summary/{}".format(i)
-        print(i)
+# Used to choose the relevant data
+def munge_data(dict):
+    return
+
+
+# Use FPL API bootstrap endpoint to get player names.
+def get_player_names():
+    r = requests.get('https://fantasy.premierleague.com/drf/bootstrap-static')
+    print('Getting Player Names...')
+
+    player = r.json()
+
+    # TODO: Create dynamic manner of counting players listed in API
+    for i in range(598):
+        PLAYER_NAMES.append(player['elements'][i]['first_name'] + ' ' + player['elements'][i]['second_name'])
+
+    print(PLAYER_NAMES)
+
+
+# Cycle through FPL API endpoint for individual players' past history.
+def get_player_past():
+    misses = 0
+    player_id = 595
+
+    while True:
+        player_url = 'https://fantasy.premierleague.com/drf/element-summary/{}'.format(player_id)
+        print('Grabbing Player #' + str(player_id))
         r = requests.get(player_url)
 
-        # Skip empty pages
+        # Skip broken URLs
         if r.status_code != 200:
-            errors[i] = r.status_code
-            print("Not found.\n")
+            misses += 1
+            print('BROKEN URL @ PLAYER #: ' + str(player_id) + '\n')
+            player_id += 1
+            # More than one miss in a row - end of requests!
+            if misses > 1:
+                print('Two broken URls in a row... Were (likely) through all the players!')
+                break
             continue
 
-        # Put player name as dictionary key. Remaining data is the key for each entry
-        temp = r.json()
-        print(temp)
+        # Reset 'missing' counter.
+        misses = 0
 
-get_old_stats(5)
+        try:
+            PLAYER_DATA_DICT[PLAYER_NAMES[player_id-1]] = r.json()
+        except ValueError:
+            print('FAILED TO PARSE PLAYER #' + str(player_id) + '\n')
+
+        player_id += 1
+
+
+get_player_names()
+print()
+get_player_past()
+print(PLAYER_DATA_DICT)
