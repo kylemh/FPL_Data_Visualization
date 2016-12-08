2a8b3e8cbd24c4aaef5f505c0ad7d772c69bed22
D <duskykmh@gmail.com>
2016-12-07 23:23:44 -0800

    Data successfully writing to CSV files. TODO: Add Player CSV for names. ENHANCEMENT: Sort CSV after write from within program.

--- FPL CSV Converter/fpl_csv_converter.py
+++ FPL CSV Converter/fpl_csv_converter.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import json
 import csv
 
 PLAYER_NAMES = []
@@ -9,15 +10,8 @@ CURR_SEASON_STATS = {}
 
 # Use FPL API bootstrap endpoint to get player names and current season stats.
 # Also used to define number of players in catalogued in FPL (to avoid unnecessary HTTP requests)
-<<<<<<< HEAD
-def get_player_names():
-    r = requests.get('https://fantasy.premierleague.com/drf/bootstrap-static')
-    print('\n...Getting player names and current season stats...')
-
-=======
 def get_curr_stats():
     r = requests.get('https://fantasy.premierleague.com/drf/bootstrap-static')
->>>>>>> b570d567d1787503ac6bafcd958cc2561b87bd95
     bootstrap_data = r.json()
     global NUM_PLAYERS
     NUM_PLAYERS = len(bootstrap_data['elements'])
@@ -27,25 +21,9 @@ def get_curr_stats():
         PLAYER_NAMES.append(bootstrap_data['elements'][i]['first_name']
                             + ' '
                             + bootstrap_data['elements'][i]['second_name'])
-        curr_season_data = {'Season 2016/2017':   bootstrap_data['elements'][i]['id']
-                                                + bootstrap_data['elements'][i]['minutes']
-                                                + bootstrap_data['elements'][i]['goals_scored']
-                                                + bootstrap_data['elements'][i]['assists']
-                                                + bootstrap_data['elements'][i]['clean_sheets']
-                                                + bootstrap_data['elements'][i]['goals_conceded']
-                                                + bootstrap_data['elements'][i]['own_goals']
-                                                + bootstrap_data['elements'][i]['penalties_saved']
-                                                + bootstrap_data['elements'][i]['penalties_missed']
-                                                + bootstrap_data['elements'][i]['yellow_cards']
-                                                + bootstrap_data['elements'][i]['red_cards']
-                                                + bootstrap_data['elements'][i]['saves']
-                                                + bootstrap_data['elements'][i]['ea_index']
-                                                + bootstrap_data['elements'][i][int('ict_index')]
-                                                + bootstrap_data['elements'][i][float('selected_by_percent')]}
-        CURR_SEASON_STATS[PLAYER_NAMES[i - 1]] = curr_season_data
 
         # Get Player's Current Season Stats
-        curr_season_data = {'id':  bootstrap_data['elements'][i]['id'],
+        curr_season_data = {'pid':  bootstrap_data['elements'][i]['id'],
                             'minutes': bootstrap_data['elements'][i]['minutes'],
                             'goals_scored': bootstrap_data['elements'][i]['goals_scored'],
                             'assists': bootstrap_data['elements'][i]['assists'],
@@ -65,12 +43,10 @@ def get_curr_stats():
                             'selected_by_percent': float(bootstrap_data['elements'][i]['selected_by_percent'])}
         CURR_SEASON_STATS[PLAYER_NAMES[i]] = curr_season_data
 
-    print(PLAYER_NAMES)
-
 
 # If a player has data for past seasons in the API endpoint, call this function
 # psc = past season count
-def get_past_season_stats(data, psc):
+def get_past_season_stats(pid, data, psc):
     previous_pl_seasons = psc
 
     # Variable declarations for summed past statistic headers
@@ -93,7 +69,8 @@ def get_past_season_stats(data, psc):
         ea_index += data['history_past'][s]['ea_index']
 
     # Create JSON of overall past season stats
-    past_season_data = {'minutes': minutes,
+    past_season_data = {'pid': pid,
+                        'minutes': minutes,
                         'goals_scored': goals_scored,
                         'assists': assists,
                         'clean_sheets': clean_sheets,
@@ -117,16 +94,9 @@ def get_past_stats():
     misses = 0
     player_url = 'https://fantasy.premierleague.com/drf/element-summary/{}'
 
-<<<<<<< HEAD
-    # Convert range from i to NUM_PLAYERS+1 when running complete
-    for i in range(590, NUM_PLAYERS + 1):
-        r = requests.get(player_url.format(i))
-        print('Grabbing Player #' + str(i))
-=======
-    for pid in range(1, NUM_PLAYERS + 1):
+    for pid in range(595, NUM_PLAYERS + 1):
         r = requests.get(player_url.format(pid))
         print('Grabbing Player #' + str(pid))
->>>>>>> b570d567d1787503ac6bafcd958cc2561b87bd95
 
         # Skip broken URLs
         if r.status_code != 200:
@@ -141,51 +111,55 @@ def get_past_stats():
         # Reset 'missing' counter to continue loop through entire API endpoint.
         misses = 0
 
-<<<<<<< HEAD
-        player_json = r.json()
-        parsed_player_data = {'Past Seasons Stats': player_json['history_past']}
-
-        PAST_SEASON_STATS[PLAYER_NAMES[i - 1]] = parsed_player_data
-        i += 1
-
-
-# ############ #
-# Main Program #
-################
-get_player_names()
-print()
-get_player_json()
-
-for player in PAST_SEASON_STATS:
-    if len(PAST_SEASON_STATS[player]['Past Seasons Stats']) == 0:
-        print(player)
-        print('Games This Season : ', PAST_SEASON_STATS[player]['Games This Season'])
-        print('Future Fixtures : ', PAST_SEASON_STATS[player]['Future Fixtures'])
-        print()
-    else:
-        print(player)
-        for json in PAST_SEASON_STATS[player]:
-            print(json, ':', PAST_SEASON_STATS[player][json])
-        print()
-=======
         player_past_json = r.json()
         past_season_count = len(player_past_json['history_past'])
 
         # If player has played in PL before...
         if past_season_count > 0:
-            PAST_SEASON_STATS[PLAYER_NAMES[pid - 1]] = get_past_season_stats(player_past_json, past_season_count)
+            PAST_SEASON_STATS[PLAYER_NAMES[pid - 1]] = get_past_season_stats(pid, player_past_json, past_season_count)
 
         # Next player...
         pid += 1
 
 
-# Main
-print('\n...Getting player names and current season stats...\n')
-get_curr_stats()
-print('\n...Getting players\' past season stats (if any)...\n')
-get_past_stats()
-print('\n...Printing sample data...\n')
+# Convert CURR_SEASON_STATS into CSV file with headers
+# fn = file name | jd = JSON or dictionary data
+def curr_dict_to_csv(jd, fn):
+    with open(fn, 'w') as csvfile:
+        headers = ['pid','minutes', 'goals_scored', 'assists', 'clean_sheets', 'goals_conceded', 'own_goals',
+                   'penalties_saved', 'penalties_missed', 'yellow_cards', 'red_cards', 'saves', 'ea_index',
+                   'influence', 'creativity', 'threat', 'ict_index', 'selected_by_percent']
+        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        writer.writeheader()
+        for i in jd:
+            writer.writerow(jd[i])
 
+
+# Convert PAST_SEASON_STATS into CSV file with headers
+# fn = file name | jd = JSON or dictionary
+def past_dict_to_csv(jd, fn):
+    with open(fn, 'w') as csvfile:
+        headers = ['pid', 'minutes', 'goals_scored', 'assists', 'clean_sheets', 'goals_conceded', 'own_goals',
+                   'penalties_saved', 'penalties_missed', 'yellow_cards', 'red_cards', 'saves', 'ea_index',
+                   'previous_pl_seasons']
+        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        writer.writeheader()
+        for i in jd:
+            writer.writerow(jd[i])
+
+
+# Main
+print('...Getting player names and current season stats...\n')
+get_curr_stats()
+print('...Getting players\' past season stats (if any)...\n')
+get_past_stats()
+print('\n...Writing CSV Files...\n')
+
+curr_dict_to_csv(CURR_SEASON_STATS, 'CurrentSeasonStats.csv')
+past_dict_to_csv(PAST_SEASON_STATS, 'PastOverallStats.csv')
+
+
+# print('\n...Printing sample data...\n')
 # Test Print - CURR_SEASON_STATS
 # print("\nTHE TOP GOAL SCORERS THIS SEASON!\n")
 # for player in CURR_SEASON_STATS:
@@ -197,4 +171,3 @@ print('\n...Printing sample data...\n')
 # for player in PAST_SEASON_STATS:
 #     if PAST_SEASON_STATS[player]['goals_scored'] > 100:
 #         print(player, ' scored ', PAST_SEASON_STATS[player]['goals_scored'], ' goals.')
->>>>>>> b570d567d1787503ac6bafcd958cc2561b87bd95
