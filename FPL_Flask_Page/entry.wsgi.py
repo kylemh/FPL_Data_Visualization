53faa93d78e1c9ec99fec59adb4570da995e4bfd
D <duskykmh@gmail.com>
2016-12-13 08:25:02 -0800

    Reset to Zoe's Commit

--- FPL_Flask_Page/entry.wsgi.py
+++ FPL_Flask_Page/entry.wsgi.py
@@ -1,3 +1,9 @@
+"""
+Authors: Zoe Olson and Kyle Holmberg
+Email: zoeo@cs.uoregon.edu or kmh@uoregon.edu
+
+Flask front-end for MySQL database-driven visualizations and queries
+"""
 from flask import Flask, request, render_template
 import pandas
 from sqlalchemy import create_engine
@@ -6,29 +12,21 @@ from sqlalchemy import create_engine
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def index():
-	con=create_engine('mysql+mysqldb://zoeo:Moraga17.@ix.cs.uoregon.edu:3640/fpl', echo=False)
-	#get data
-	#datar=pandas.read_sql('SELECT nationality FROM Player', con)
-	datar=pandas.read_sql('SELECT p.nationality, SUM(s.mins_played) AS mins_played FROM CurrentSeasonStats s left join Player p ON s.Player_pid = p.pid GROUP BY p.nationality', con)
+	con = create_engine('mysql+mysqldb://zoeo:Moraga17.@ix.cs.uoregon.edu:3640/fpl', echo=False)
+	datar = pandas.read_sql('SELECT p.nationality, SUM(s.mins_played) AS mins_played FROM CurrentSeasonStats s left join Player p ON s.Player_pid = p.pid GROUP BY p.nationality', con)
 	return render_template('index.html', table=datar)
-
 
 
 @app.route('/players', methods = ['GET', 'POST'])
 def players():
-	con=create_engine('mysql+mysqldb://zoeo:Moraga17.@ix.cs.uoregon.edu:3640/fpl', echo=False)
+	con = create_engine('mysql+mysqldb://zoeo:Moraga17.@ix.cs.uoregon.edu:3640/fpl', echo=False)
 	country = request.form['country']
-	data=pandas.read_sql("SELECT name, nationality FROM Player WHERE nationality LIKE '" + country + "'", con)
-	#cursor.execute("SELECT id, nationality FROM players WHERE nationality LIKE '" + country + "'")
-	#entries = [dict(id=row[0], nationality=row[1]) for row in datar]
+	data = pandas.read_sql("SELECT name, nationality FROM Player WHERE nationality LIKE '" + country + "'", con)
 	entries = data.T.to_dict().values()
 	return render_template('players.html', entries = entries)
 
 
-
-
 if __name__ == "__main__":
-	app.run(host='128.223.4.35', port=5558, debug=True)
+	app.run(debug=True)
