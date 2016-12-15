9e59bade77bece6d1a91bb18f7f07be1e8e6114e
D <duskykmh@gmail.com>
2016-12-14 22:46:06 -0800

    Added host randomizing, changed mysqldb to pymysql, and added requirements.txt

--- FPL_Flask_Page/entry.wsgi.py
+++ FPL_Flask_Page/entry.wsgi.py
@@ -7,6 +7,7 @@ Flask front-end for MySQL database-driven visualizations and queries
 from flask import Flask, request, render_template
 import pandas
 from sqlalchemy import create_engine
+import random
 
 
 app = Flask(__name__)
@@ -14,19 +15,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	con = create_engine('mysql+mysqldb://guest:guest@ix.cs.uoregon.edu:3640/fpl', echo=False)
+	con = create_engine('mysql+pymysql://guest:guest@ix.cs.uoregon.edu:3640/fpl', echo=False)
 	datar = pandas.read_sql('SELECT p.nationality, SUM(s.mins_played) AS mins_played FROM CurrentSeasonStats s left join Player p ON s.Player_pid = p.pid GROUP BY p.nationality', con)
 	return render_template('index.html', table=datar)
 
 
-@app.route('/players', methods = ['GET', 'POST'])
+@app.route('/players', methods=['GET', 'POST'])
 def players():
-	con = create_engine('mysql+mysqldb://guest:guest@ix.cs.uoregon.edu:3640/fpl', echo=False)
+	con = create_engine('mysql+pymysql://guest:guest@ix.cs.uoregon.edu:3640/fpl', echo=False)
 	country = request.form['country']
 	data = pandas.read_sql("SELECT name, nationality FROM Player WHERE nationality LIKE '" + country + "'", con)
 	entries = data.T.to_dict().values()
-	return render_template('players.html', entries = entries)
+	return render_template('players.html', entries=entries)
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	random_port = random.randint(1000, 5000)  # In order to "ensure" no hosting overlaps
+	app.run(host='128.223.4.35', port=random_port, debug=True)
