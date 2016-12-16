fb97eaff863b1af6e04c11e978706d20be72648d
D <duskykmh@gmail.com>
2016-12-16 01:48:22 -0800

    Added more functionality.

--- FPL_Flask_Page/entry.wsgi.py
+++ FPL_Flask_Page/entry.wsgi.py
@@ -1,13 +1,6 @@
-"""
-Authors: Zoe Olson and Kyle Holmberg
-Email: zoeo@cs.uoregon.edu or kmh@uoregon.edu
-
-Flask front-end for MySQL database-driven visualizations and queries
-"""
 from flask import Flask, request, render_template
 import pandas
 from sqlalchemy import create_engine
-import random
 
 
 app = Flask(__name__)
@@ -26,9 +19,26 @@ def players():
 	country = request.form['country']
 	data = pandas.read_sql("SELECT name, nationality FROM Player WHERE nationality LIKE '" + country + "'", con)
 	entries = data.T.to_dict().values()
-	return render_template('players.html', entries=entries)
+	return render_template('players.html', entries=entries, user_input=country)
+
+
+@app.route('/goals', methods=['GET', 'POST'])
+def goals():
+	con=create_engine('mysql+pymysql://guest:guest@ix.cs.uoregon.edu:3640/fpl', echo=False)
+	goal = request.form['goal']
+	data = pandas.read_sql("SELECT p.name, r.gameweek, s.goals_scored FROM Player p LEFT JOIN PlayerResultStats s ON p.pid = s.Player_pid LEFT JOIN Result r ON s.Result_id = r.id Where s.goals_scored >= '" + goal + "'", con)
+	entries = data.T.to_dict().values()
+	return render_template('goals.html', entries=entries)
+
+
+@app.route('/stats', methods=['GET', 'POST'])
+def stats():
+	con=create_engine('mysql+pymysql://guest:guest@ix.cs.uoregon.edu:3640/fpl', echo=False)
+	stat = request.form['stat']
+	data = pandas.read_sql("SELECT p.name, p.position, c.total_points, c.mins_played, c.goals_scored, c.assists, c.clean_sheets, c.own_goals, c.penalties_saved, c.penalties_missed, c.yellow_cards, c.red_cards, c.saves, c.ea_index, c.influence, c.creativity, c.threat, c.ict_index FROM Player p LEFT JOIN CurrentSeasonStats c ON p.pid = c.Player_pid WHERE p.name like '" + stat + "'", con)
+	entries = data.T.to_dict().values()
+	return render_template('stats.html', entries=entries)
 
 
 if __name__ == "__main__":
-	random_port = random.randint(1000, 5000)  # In order to "ensure" no hosting overlaps
-	app.run(host='128.223.4.35', port=random_port, debug=True)
+	app.run(host='0.0.0.0', port=2000, debug=True)
